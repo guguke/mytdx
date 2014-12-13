f476376ea02b4f2af00131ab8ae9abab9d0d3ae5
guguke <gbm618@gmail.com>
2014-12-13 15:55:07 +0800

    bat

---
+++ cp060079.bat
@@ -0,0 +1,99 @@
+rem 成交额 20140718
+copy X:\new_zszq\T0002\blocknew\.084.blk X:\new_zszq\T0002\blocknew\.085.blk
+copy X:\new_zszq\T0002\blocknew\.083.blk X:\new_zszq\T0002\blocknew\.084.blk
+copy X:\new_zszq\T0002\blocknew\.082.blk X:\new_zszq\T0002\blocknew\.083.blk
+copy X:\new_zszq\T0002\blocknew\.081.blk X:\new_zszq\T0002\blocknew\.082.blk
+copy X:\new_zszq\T0002\blocknew\.080.blk X:\new_zszq\T0002\blocknew\.081.blk
+
+copy X:\new_zszq\T0002\blocknew\.078.blk X:\new_zszq\T0002\blocknew\.079.blk
+copy X:\new_zszq\T0002\blocknew\.077.blk X:\new_zszq\T0002\blocknew\.078.blk
+copy X:\new_zszq\T0002\blocknew\.076.blk X:\new_zszq\T0002\blocknew\.077.blk
+copy X:\new_zszq\T0002\blocknew\.075.blk X:\new_zszq\T0002\blocknew\.076.blk
+copy X:\new_zszq\T0002\blocknew\.074.blk X:\new_zszq\T0002\blocknew\.075.blk
+copy X:\new_zszq\T0002\blocknew\.073.blk X:\new_zszq\T0002\blocknew\.074.blk
+copy X:\new_zszq\T0002\blocknew\.072.blk X:\new_zszq\T0002\blocknew\.073.blk
+copy X:\new_zszq\T0002\blocknew\.071.blk X:\new_zszq\T0002\blocknew\.072.blk
+copy X:\new_zszq\T0002\blocknew\.070.blk X:\new_zszq\T0002\blocknew\.071.blk
+
+copy X:\new_zszq\T0002\blocknew\.069.blk X:\new_zszq\T0002\blocknew\.070.blk
+copy X:\new_zszq\T0002\blocknew\.068.blk X:\new_zszq\T0002\blocknew\.069.blk
+copy X:\new_zszq\T0002\blocknew\.067.blk X:\new_zszq\T0002\blocknew\.068.blk
+copy X:\new_zszq\T0002\blocknew\.066.blk X:\new_zszq\T0002\blocknew\.067.blk
+copy X:\new_zszq\T0002\blocknew\.065.blk X:\new_zszq\T0002\blocknew\.066.blk
+copy X:\new_zszq\T0002\blocknew\.064.blk X:\new_zszq\T0002\blocknew\.065.blk
+copy X:\new_zszq\T0002\blocknew\.063.blk X:\new_zszq\T0002\blocknew\.064.blk
+copy X:\new_zszq\T0002\blocknew\.062.blk X:\new_zszq\T0002\blocknew\.063.blk
+copy X:\new_zszq\T0002\blocknew\.061.blk X:\new_zszq\T0002\blocknew\.062.blk
+copy X:\new_zszq\T0002\blocknew\.060.blk X:\new_zszq\T0002\blocknew\.061.blk
+
+rem 5-2天跌HL最大 2014.10.09
+rem copy X:\new_zszq\T0002\blocknew\..14.blk X:\new_zszq\T0002\blocknew\..15.blk
+rem copy X:\new_zszq\T0002\blocknew\..13.blk X:\new_zszq\T0002\blocknew\..14.blk
+rem copy X:\new_zszq\T0002\blocknew\..12.blk X:\new_zszq\T0002\blocknew\..13.blk
+rem copy X:\new_zszq\T0002\blocknew\..11.blk X:\new_zszq\T0002\blocknew\..12.blk
+rem copy X:\new_zszq\T0002\blocknew\..10.blk X:\new_zszq\T0002\blocknew\..11.blk
+
+rem 跌幅榜
+copy X:\new_zszq\T0002\blocknew\..28.blk X:\new_zszq\T0002\blocknew\..29.blk
+copy X:\new_zszq\T0002\blocknew\..27.blk X:\new_zszq\T0002\blocknew\..28.blk
+copy X:\new_zszq\T0002\blocknew\..26.blk X:\new_zszq\T0002\blocknew\..27.blk
+copy X:\new_zszq\T0002\blocknew\..25.blk X:\new_zszq\T0002\blocknew\..26.blk
+copy X:\new_zszq\T0002\blocknew\..24.blk X:\new_zszq\T0002\blocknew\..25.blk
+copy X:\new_zszq\T0002\blocknew\..23.blk X:\new_zszq\T0002\blocknew\..24.blk
+copy X:\new_zszq\T0002\blocknew\..22.blk X:\new_zszq\T0002\blocknew\..23.blk
+copy X:\new_zszq\T0002\blocknew\..21.blk X:\new_zszq\T0002\blocknew\..22.blk
+copy X:\new_zszq\T0002\blocknew\..20.blk X:\new_zszq\T0002\blocknew\..21.blk
+
+copy X:\new_zszq\T0002\blocknew\..48.blk X:\new_zszq\T0002\blocknew\..49.blk
+copy X:\new_zszq\T0002\blocknew\..47.blk X:\new_zszq\T0002\blocknew\..48.blk
+copy X:\new_zszq\T0002\blocknew\..46.blk X:\new_zszq\T0002\blocknew\..47.blk
+copy X:\new_zszq\T0002\blocknew\..45.blk X:\new_zszq\T0002\blocknew\..46.blk
+copy X:\new_zszq\T0002\blocknew\..44.blk X:\new_zszq\T0002\blocknew\..45.blk
+copy X:\new_zszq\T0002\blocknew\..43.blk X:\new_zszq\T0002\blocknew\..44.blk
+copy X:\new_zszq\T0002\blocknew\..42.blk X:\new_zszq\T0002\blocknew\..43.blk
+copy X:\new_zszq\T0002\blocknew\..41.blk X:\new_zszq\T0002\blocknew\..42.blk
+copy X:\new_zszq\T0002\blocknew\..40.blk X:\new_zszq\T0002\blocknew\..41.blk
+
+copy X:\new_zszq\T0002\blocknew\..78.blk X:\new_zszq\T0002\blocknew\..79.blk
+copy X:\new_zszq\T0002\blocknew\..77.blk X:\new_zszq\T0002\blocknew\..78.blk
+copy X:\new_zszq\T0002\blocknew\..76.blk X:\new_zszq\T0002\blocknew\..77.blk
+copy X:\new_zszq\T0002\blocknew\..75.blk X:\new_zszq\T0002\blocknew\..76.blk
+copy X:\new_zszq\T0002\blocknew\..74.blk X:\new_zszq\T0002\blocknew\..75.blk
+copy X:\new_zszq\T0002\blocknew\..73.blk X:\new_zszq\T0002\blocknew\..74.blk
+copy X:\new_zszq\T0002\blocknew\..72.blk X:\new_zszq\T0002\blocknew\..73.blk
+copy X:\new_zszq\T0002\blocknew\..71.blk X:\new_zszq\T0002\blocknew\..72.blk
+copy X:\new_zszq\T0002\blocknew\..70.blk X:\new_zszq\T0002\blocknew\..71.blk
+
+
+copy X:\new_zszq\T0002\blocknew\.058.blk X:\new_zszq\T0002\blocknew\.059.blk
+copy X:\new_zszq\T0002\blocknew\.057.blk X:\new_zszq\T0002\blocknew\.058.blk
+copy X:\new_zszq\T0002\blocknew\.056.blk X:\new_zszq\T0002\blocknew\.057.blk
+copy X:\new_zszq\T0002\blocknew\.055.blk X:\new_zszq\T0002\blocknew\.056.blk
+copy X:\new_zszq\T0002\blocknew\.054.blk X:\new_zszq\T0002\blocknew\.055.blk
+copy X:\new_zszq\T0002\blocknew\.053.blk X:\new_zszq\T0002\blocknew\.054.blk
+copy X:\new_zszq\T0002\blocknew\.052.blk X:\new_zszq\T0002\blocknew\.053.blk
+copy X:\new_zszq\T0002\blocknew\.051.blk X:\new_zszq\T0002\blocknew\.052.blk
+copy X:\new_zszq\T0002\blocknew\.050.blk X:\new_zszq\T0002\blocknew\.051.blk
+
+copy X:\new_zszq\T0002\blocknew\.049.blk X:\new_zszq\T0002\blocknew\.050.blk
+copy X:\new_zszq\T0002\blocknew\.048.blk X:\new_zszq\T0002\blocknew\.049.blk
+copy X:\new_zszq\T0002\blocknew\.047.blk X:\new_zszq\T0002\blocknew\.048.blk
+copy X:\new_zszq\T0002\blocknew\.046.blk X:\new_zszq\T0002\blocknew\.047.blk
+copy X:\new_zszq\T0002\blocknew\.045.blk X:\new_zszq\T0002\blocknew\.046.blk
+copy X:\new_zszq\T0002\blocknew\.044.blk X:\new_zszq\T0002\blocknew\.045.blk
+copy X:\new_zszq\T0002\blocknew\.043.blk X:\new_zszq\T0002\blocknew\.044.blk
+copy X:\new_zszq\T0002\blocknew\.042.blk X:\new_zszq\T0002\blocknew\.043.blk
+copy X:\new_zszq\T0002\blocknew\.041.blk X:\new_zszq\T0002\blocknew\.042.blk
+copy X:\new_zszq\T0002\blocknew\.040.blk X:\new_zszq\T0002\blocknew\.041.blk
+
+copy X:\new_zszq\T0002\blocknew\..94.blk X:\new_zszq\T0002\blocknew\..95.blk
+copy X:\new_zszq\T0002\blocknew\..93.blk X:\new_zszq\T0002\blocknew\..94.blk
+copy X:\new_zszq\T0002\blocknew\..92.blk X:\new_zszq\T0002\blocknew\..93.blk
+copy X:\new_zszq\T0002\blocknew\..91.blk X:\new_zszq\T0002\blocknew\..92.blk
+
+copy X:\new_zszq\T0002\blocknew\.085.blk X:\new_zszq\T0002\blocknew\.095.blk
+copy X:\new_zszq\T0002\blocknew\.084.blk X:\new_zszq\T0002\blocknew\.094.blk
+copy X:\new_zszq\T0002\blocknew\.083.blk X:\new_zszq\T0002\blocknew\.093.blk
+copy X:\new_zszq\T0002\blocknew\.082.blk X:\new_zszq\T0002\blocknew\.092.blk
+copy X:\new_zszq\T0002\blocknew\.081.blk X:\new_zszq\T0002\blocknew\.091.blk
+
---
+++ del2000zs.bat
@@ -0,0 +1,50 @@
+rem 2013.08.24 H5L H4L H3L H2L H1L max
+copy X:\new_zszq\T0002\blocknew\blk.blk X:\new_zszq\T0002\blocknew\TJG.blk
+
+copy X:\new_zszq\T0002\blocknew\blk.blk X:\new_zszq\T0002\blocknew\.060.blk
+copy X:\new_zszq\T0002\blocknew\blk.blk X:\new_zszq\T0002\blocknew\.080.blk
+rem copy X:\new_zszq\T0002\blocknew\blk.blk X:\new_zszq\T0002\blocknew\.081.blk
+rem copy X:\new_zszq\T0002\blocknew\blk.blk X:\new_zszq\T0002\blocknew\.082.blk
+rem copy X:\new_zszq\T0002\blocknew\blk.blk X:\new_zszq\T0002\blocknew\.083.blk
+rem copy X:\new_zszq\T0002\blocknew\blk.blk X:\new_zszq\T0002\blocknew\.084.blk
+rem copy X:\new_zszq\T0002\blocknew\blk.blk X:\new_zszq\T0002\blocknew\.085.blk
+
+copy X:\new_zszq\T0002\blocknew\blk.blk X:\new_zszq\T0002\blocknew\..10.blk
+copy X:\new_zszq\T0002\blocknew\blk.blk X:\new_zszq\T0002\blocknew\..11.blk
+copy X:\new_zszq\T0002\blocknew\blk.blk X:\new_zszq\T0002\blocknew\..12.blk
+copy X:\new_zszq\T0002\blocknew\blk.blk X:\new_zszq\T0002\blocknew\..13.blk
+copy X:\new_zszq\T0002\blocknew\blk.blk X:\new_zszq\T0002\blocknew\..14.blk
+copy X:\new_zszq\T0002\blocknew\blk.blk X:\new_zszq\T0002\blocknew\..15.blk
+
+copy X:\new_zszq\T0002\blocknew\blk.blk X:\new_zszq\T0002\blocknew\.040.blk
+
+copy X:\new_zszq\T0002\blocknew\blk.blk X:\new_zszq\T0002\blocknew\..20.blk
+
+rem copy X:\new_zszq\T0002\blocknew\blk.blk X:\new_zszq\T0002\blocknew\..30.blk
+rem copy X:\new_zszq\T0002\blocknew\blk.blk X:\new_zszq\T0002\blocknew\..31.blk
+rem copy X:\new_zszq\T0002\blocknew\blk.blk X:\new_zszq\T0002\blocknew\..32.blk
+rem copy X:\new_zszq\T0002\blocknew\blk.blk X:\new_zszq\T0002\blocknew\..33.blk
+copy X:\new_zszq\T0002\blocknew\blk.blk X:\new_zszq\T0002\blocknew\..40.blk
+rem copy X:\new_zszq\T0002\blocknew\blk.blk X:\new_zszq\T0002\blocknew\..41.blk
+rem copy X:\new_zszq\T0002\blocknew\blk.blk X:\new_zszq\T0002\blocknew\..42.blk
+copy X:\new_zszq\T0002\blocknew\blk.blk X:\new_zszq\T0002\blocknew\..50.blk
+copy X:\new_zszq\T0002\blocknew\blk.blk X:\new_zszq\T0002\blocknew\..51.blk
+copy X:\new_zszq\T0002\blocknew\blk.blk X:\new_zszq\T0002\blocknew\..52.blk
+
+copy X:\new_zszq\T0002\blocknew\blk.blk X:\new_zszq\T0002\blocknew\..70.blk
+
+copy X:\new_zszq\T0002\blocknew\blk.blk X:\new_zszq\T0002\blocknew\..89.blk
+copy X:\new_zszq\T0002\blocknew\blk.blk X:\new_zszq\T0002\blocknew\..88.blk
+copy X:\new_zszq\T0002\blocknew\blk.blk X:\new_zszq\T0002\blocknew\..87.blk
+
+copy X:\new_zszq\T0002\blocknew\blk.blk X:\new_zszq\T0002\blocknew\..91.blk
+copy X:\new_zszq\T0002\blocknew\blk.blk X:\new_zszq\T0002\blocknew\..96.blk
+copy X:\new_zszq\T0002\blocknew\blk.blk X:\new_zszq\T0002\blocknew\..90.blk
+copy X:\new_zszq\T0002\blocknew\blk.blk X:\new_zszq\T0002\blocknew\..97.blk
+rem copy X:\new_zszq\T0002\blocknew\blk.blk X:\new_zszq\T0002\blocknew\..09.blk
+
+rem copy X:\new_zszq\T0002\blocknew\blk.blk X:\new_zszq\T0002\blocknew\..86.blk
+rem copy X:\new_zszq\T0002\blocknew\blk.blk X:\new_zszq\T0002\blocknew\..80.blk
+rem copy X:\new_zszq\T0002\blocknew\blk.blk X:\new_zszq\T0002\blocknew\..19.blk
+
+
